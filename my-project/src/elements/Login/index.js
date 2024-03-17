cf0c66faf12f894dfd1132873dfc0d6f57170a1a
Vitor Prosperi <145865047+vitorprosperi@users.noreply.github.com>
2024-03-17 20:20:11 -0300

    FIrebase_SignUp

--- my-project/src/elements/Login/index.js
+++ my-project/src/elements/Login/index.js
@@ -1,20 +1,44 @@
-import React from "react";
-import {View, Text, TextInput, Button} from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput, Button } from "react-native";
+import auth from "@react-native-firebase/auth";
 
-export default function Form(){
-    return(
+export default function Form() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  function signUp(){
+    auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(userCredential => {
+        console.log('user: ', userCredential);
+      }).catch(error =>{
+        if(error.code ==='auth/email-already-in-use'){
+          console.log('E-MAIL JÁ CADASTRADO');
+        }
+        if(error.code ==='auth/invalid-email'){
+          console.log('E-MAIL INVÁLIDO');
+        }
+      });
+  }
+
+  return (
+    <View>
       <View>
-        <View>
-            <Text>E-MAIL</Text>
-            <TextInput
-            placeholder="teste.teste@gmail.com"
-            />
-            <Text>SENHA</Text>
-            <TextInput
-            placeholder="suasenhasupersegura"
-            />
-            <Button title="entrar"/>
-        </View>
+        <Text>E-MAIL</Text>
+        <TextInput
+          placeholder="teste.teste@gmail.com"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Text>SENHA</Text>
+        <TextInput
+          placeholder="suasenhasupersegura"
+          secureTextEntry={true}
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+        <Button title="Cadastrar" onPress={signUp} />
       </View>
-    );
+    </View>
+  );
 }
