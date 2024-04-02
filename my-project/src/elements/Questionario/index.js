f4491b8ec9b5d74c1fffe9e3e1ca2b758cae4223
Vitor Prosperi <145865047+vitorprosperi@users.noreply.github.com>
2024-04-02 13:31:05 -0300

    Questionario1.1
    Regresso.

--- my-project/src/elements/Questionario/index.js
+++ my-project/src/elements/Questionario/index.js
@@ -6,85 +6,65 @@ import { collection, getDocs } from "firebase/firestore";
 
 export function Questionario() {
     const [perguntas, setPerguntas] = useState([]);
-    const [respostas, setRespostas] = useState([]);
 
     useEffect(() => {
-        const fetchData = async () => {
+        const fetchPerguntas = async () => {
             try {
                 const perguntasSnapshot = await getDocs(collection(db, 'perguntas'));
                 const perguntasData = perguntasSnapshot.docs.map(doc => doc.data());
                 setPerguntas(perguntasData);
-                const initialRespostas = perguntasData.map(() => [0, 0, 0]); // Inicializa as respostas de cada pergunta como [0, 0, 0]
-                setRespostas(initialRespostas);
             } catch (error) {
                 console.error('Erro ao buscar perguntas:', error);
             }
         };
-        fetchData();
+        fetchPerguntas();
     }, []);
 
     const handleRespostaChange = (perguntaIndex, opcaoIndex, resposta) => {
-        const updatedRespostas = [...respostas];
-        updatedRespostas[perguntaIndex][opcaoIndex] = resposta;
-        setRespostas(updatedRespostas);
-    };
-
-    const enviarRespostas = () => {
-        console.log('Respostas enviadas:', respostas);
+        // Aqui você pode adicionar lógica para lidar com a mudança de resposta
+        console.log(`Pergunta ${perguntaIndex + 1}, Opção ${opcaoIndex + 1}, Resposta ${resposta}`);
     };
 
     return (
         <View style={styles.container}>
             <Text>Antes de Começarmos, Algumas Perguntas Serão Feitas!</Text>
-            {perguntas.map((pergunta, perguntaIndex) => (
-                <View key={perguntaIndex}>
-                    <Text>{perguntaIndex + 1}: {pergunta.perg1}</Text>
+            {perguntas.map((pergunta, index) => (
+                <View key={index}>
+                    <Text>{index + 1}: {pergunta.perg1}</Text>
                     <View style={styles.botaoContainer}>
                         {[...Array(10)].map((_, numero) => (
                             <TouchableOpacity
                                 key={numero}
-                                style={[
-                                    styles.botao,
-                                    respostas[perguntaIndex][0] === numero + 1 ? styles.botaoPressionado : null
-                                ]}
-                                onPress={() => handleRespostaChange(perguntaIndex, 0, numero + 1)}>
+                                style={styles.botao}
+                                onPress={() => handleRespostaChange(index, 0, numero + 1)}>
                                 <Text style={styles.botaoTexto}>{numero + 1}</Text>
                             </TouchableOpacity>
                         ))}
                     </View>
-                    <Text>{perguntaIndex + 1}: {pergunta.perg2}</Text>
+                    <Text>{index + 2}: {pergunta.perg2}</Text>
                     <View style={styles.botaoContainer}>
                         {[...Array(10)].map((_, numero) => (
                             <TouchableOpacity
                                 key={numero}
-                                style={[
-                                    styles.botao,
-                                    respostas[perguntaIndex][1] === numero + 1 ? styles.botaoPressionado : null
-                                ]}
-                                onPress={() => handleRespostaChange(perguntaIndex, 1, numero + 1)}>
+                                style={styles.botao}
+                                onPress={() => handleRespostaChange(index, 1, numero + 1)}>
                                 <Text style={styles.botaoTexto}>{numero + 1}</Text>
                             </TouchableOpacity>
                         ))}
                     </View>
-                    <Text>{perguntaIndex + 1}: {pergunta.perg3}</Text>
+                    <Text>{index + 3}: {pergunta.perg3}</Text>
                     <View style={styles.botaoContainer}>
                         {[...Array(10)].map((_, numero) => (
                             <TouchableOpacity
                                 key={numero}
-                                style={[
-                                    styles.botao,
-                                    respostas[perguntaIndex][2] === numero + 1 ? styles.botaoPressionado : null
-                                ]}
-                                onPress={() => handleRespostaChange(perguntaIndex, 2, numero + 1)}>
+                                style={styles.botao}
+                                onPress={() => handleRespostaChange(index, 2, numero + 1)}>
                                 <Text style={styles.botaoTexto}>{numero + 1}</Text>
                             </TouchableOpacity>
                         ))}
                     </View>
                 </View>
             ))}
-            <TouchableOpacity style={styles.buttonEnviar} onPress={enviarRespostas}>
-                <Text style={styles.buttonText}>Enviar Respostas</Text>
-            </TouchableOpacity>
         </View>
     );
 }
