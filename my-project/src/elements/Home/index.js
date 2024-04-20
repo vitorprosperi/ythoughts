6fd688896431192bdfadf6bb6af8c04bba9d569d
Vitor Prosperi <145865047+vitorprosperi@users.noreply.github.com>
2024-04-20 13:44:28 -0300

    Adicionando notas
    Adicionada Botão de Adicionar

--- my-project/src/elements/Home/index.js
+++ my-project/src/elements/Home/index.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { View, Text, Button, Image, StyleSheet } from "react-native";
+import { View, Text, Button, Image, StyleSheet, TouchableOpacity } from "react-native";
 import { useNavigation } from '@react-navigation/native';
 import { auth } from "../../../Firebase/FirebaseConnection";
 
 export default function Home(){
     const navigation = useNavigation();
+
+    function abrirform(){
+       navigation.navigate('addform');
+    }
 
     const signOut = () => {
         auth.signOut().then(() => {
@@ -17,14 +21,18 @@ export default function Home(){
 
     return(
         <View>
-            <View style={styles.container} >
-                <Image
-                    source={require('../../../tcc/imagens/mais.png')}
-                    style={styles.image}
-                />
-             </View>   
-            <Text>Vitor, Você Conseguiu!</Text>
-            <Button title="Sair" onPress={signOut} />
+            <TouchableOpacity onPress={abrirform}>
+                <View style={styles.imageContainer}>
+                    <Image
+                        source={require('../../../tcc/imagens/mais.png')}
+                        style={styles.image}
+                    />
+                </View>
+            </TouchableOpacity>
+            <View>    
+                <Text style={styles.text}>Vitor, Você Conseguiu!</Text>
+                <Button title="Sair" onPress={signOut} />
+            </View>    
         </View>
     );
 }
@@ -32,12 +40,21 @@ export default function Home(){
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'flex-end', // Alinha os itens à direita
-        justifyContent: 'center', // Centraliza verticalmente
-        paddingRight: 20, // Espaçamento à direita para evitar que a imagem fique colada à borda
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    imageContainer: {
+        position: 'absolute',
+        top: 20,
+        right: 20,
     },
     image: {
         width: 35,
         height: 35,
     },
+    text: {
+        marginTop: 50,
+        fontSize: 20,
+    },
 });
+
