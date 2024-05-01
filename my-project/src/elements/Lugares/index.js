724f095d1cc93e64797eeb7602682438fbe9a84b
Vitor Prosperi <145865047+vitorprosperi@users.noreply.github.com>
2024-05-01 12:13:39 -0300

    Tentando API

--- my-project/src/elements/Lugares/index.js
+++ my-project/src/elements/Lugares/index.js
@@ -4,8 +4,11 @@ import { Text, View, StyleSheet } from "react-native";
 import { requestForegroundPermissionsAsync, 
     getCurrentPositionAsync,
     watchPositionAsync, 
-    LocationAccuracy
+    LocationAccuracy,
+    // Importe Constants do Expo
 } from 'expo-location';
+import Constants from 'expo-constants';
+import axios from 'axios'; // Importe a biblioteca axios para fazer solicitações HTTP
 
 export default function Lugares() {
     const [location, setLocation] = useState(null);
@@ -42,6 +45,28 @@ export default function Lugares() {
         });
     }, []);
 
+    useEffect(() => {
+        // Solicitação à API do Google Places
+        const googlePlacesApiKey = Constants.manifest.extra.googlePlacesApiKey; // Obtenha a chave da API do Google Places do objeto Constants
+        const apiUrl = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json`;
+
+        if (location) {
+            axios.get(apiUrl, {
+                params: {
+                    input: 'clinic',
+                    inputtype: 'textquery',
+                    key: googlePlacesApiKey, // Use a chave da API do Google Places
+                    locationbias: `circle:5000@${location.coords.latitude},${location.coords.longitude}`
+                }
+            }).then(response => {
+                console.log(response.data);
+                // Aqui você pode processar a resposta e adicionar marcadores ao mapa
+            }).catch(error => {
+                console.error('Erro ao fazer solicitação à API do Google Places:', error);
+            });
+        }
+    }, [location]); // Execute esta solicitação sempre que a localização for atualizada
+
     return (
         <View style={styles.container}>
             {location ? (
@@ -63,12 +88,14 @@ export default function Lugares() {
                         }
                     }}
                 >
+                    {/* Marque a localização atual */}
                     <Marker
                         coordinate={{
                             latitude: location.coords.latitude,
                             longitude: location.coords.longitude,
                         }}
                     />
+                    {/* Adicione mais marcadores aqui com base na resposta da API do Google Places */}
                 </MapView>
             ) : (
                 <Text>Carregando mapa...</Text>
